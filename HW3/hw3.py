fe90675989bdc5d2c8c32026ef95b18dea1c6006
Won Steinbach <wonstein@gmail.com>
2013-01-29 14:11:47 -0500

    Coded comb_sort algorithm

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -12,3 +12,20 @@ def bubble_sort(unsorted, sorted):
     print "Round %d: %r" % (count, unsorted)
     count += 1
  
+def comb_sort(unsorted, sorted):
+  shrink = 1.3
+  #print "Unsorted arrary: %r" % unsorted
+  count = 1
+  while unsorted != sorted:
+    gap = int(len(unsorted)/shrink**count)
+    if gap < 1: gap = 1
+    for i in range(0, len(unsorted)-gap):
+      if unsorted[i] > unsorted[i+gap]:
+        unsorted.insert(i, unsorted[i+gap])
+        unsorted.pop(i+gap+1)
+        unsorted.insert(i+gap+1, unsorted[i+1])
+        unsorted.pop(i+1)
+    #print "Round %d (Gap = %d): %r" % (count, gap, unsorted)
+    count += 1
+    if unsorted == sorted: print "Done!"
+    
