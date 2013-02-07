ed1ca67544ec5caf2b999fb2ce90e0e19aa9ea84
Won Steinbach <wonstein@gmail.com>
2013-02-07 15:48:05 -0500

    Added findNode, addNodeAfter, and addNodeBefore functions

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -19,28 +19,25 @@ class LinkedList(object):
     return self.length
     
   def addNode(self, new_value):
-    #exec "self.Node%s = Node(new_value)" % self.length
-    #exec "self.Node%s.changeNext(self.Node%s)" % (self.length-1, self.length)
     current_node = self.head
     for i in range(1, self.length):
       current_node = current_node.next
     current_node.changeNext(Node(new_value))
     self.length += 1
   
+  def checkIndex(self, index):
+    if index not in range(self.length): raise Exception, "No node at this location."
+    
   def removeNode(self, node_to_remove):
-    if node_to_remove >= self.length: raise Exception, "No node at this location."
-    if node_to_remove == 0:
+    self.checkIndex(node_to_remove)
+    PriorNode = self.findNode(node_to_remove-1)
+    NodeToRemove = self.findNode(node_to_remove)
+    if NodeToRemove == self.head:
       self.head = self.head.next
     else:
-      i = 1
-      current_node = self.head
-      while i < node_to_remove:
-        current_node = current_node.next
-        i += 1
-      current_node.changeNext(current_node.next.next)
+      PriorNode.changeNext(PriorNode.next.next)
     self.length -= 1
         
-    
   def __str__(self):
     list = []
     current_node = self.head
@@ -50,19 +47,40 @@ class LinkedList(object):
     return str(list)
     
   def addNodeAfter(self, new_value, after_node):
-    #if isinstance(after_node, int): exec "after_node = self.Node%i" %after_node
-    pass  
+    self.checkIndex(after_node)
+    NodeToAppend = self.findNode(after_node)
+    NodeToAppend.changeNext(Node(new_value, NodeToAppend.next))
+    self.length += 1
+    
+  def addNodeBefore(self, new_value, before_node):
+    self.checkIndex(before_node)
+    if before_node == 0: 
+      self.head = Node(new_value, self.head)
+    else:
+      PriorNode = self.findNode(before_node-1)
+      PriorNode.changeNext(Node(new_value, PriorNode.next))
+    self.length +=1
+    
+  
+  # Finds particular node in linked list given an index
+  def findNode(self, index):  
+    current_node = self.head
+    i = 0
+    while i < index:
+      current_node = current_node.next
+      i += 1
+    return current_node
       
 ll = LinkedList(1)
 print ll # 1
 ll.addNode(2)
 print ll #12
-ll.addNode(5) 
+ll.addNode(3) 
 print ll # 123
 ll.addNode(4) #123
 print ll
-ll.addNode(74)
+ll.addNode(5)
 print ll
-ll.removeNode(5)
+ll.addNodeBefore(0, 5)
 print ll
 #print ll.Node3
