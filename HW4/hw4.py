c3153f0da06732c61c4ff3e4f9dffb38f2eb8d48
Won Steinbach <wonstein@gmail.com>
2013-02-07 17:17:16 -0500

    Added reverse function

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -11,20 +11,37 @@ class Node(object):
     
 class LinkedList(object):
   def __init__(self, value):
-    #self.Node0 = Node(value)
     self.head = Node(value)
     self.length = 1
 
   def length(self):
     return self.length
     
-  def addNode(self, new_value):
+  def __str__(self):
+    list = []
+    for i in range(self.length):
+      list.append(self.findNode(i).value)
+    return str(list)
+    
+  # Finds particular node in linked list given an index
+  def findNode(self, index):  
     current_node = self.head
-    for i in range(1, self.length):
+    i = 0
+    while i < index:
       current_node = current_node.next
-    current_node.changeNext(Node(new_value))
-    self.length += 1
+      i += 1
+    return current_node
+    
+  def addNode(self, new_value):
+    if self.length == 0: self.__init__(new_value)
+    else:
+      current_node = self.head
+      for i in range(1, self.length):
+        current_node = current_node.next
+      current_node.changeNext(Node(new_value))
+      self.length += 1
   
+  # Checks whether a given index exists in the list
   def checkIndex(self, index):
     if index not in range(self.length): raise Exception, "No node at this location."
     
@@ -37,14 +54,6 @@ class LinkedList(object):
     else:
       PriorNode.changeNext(PriorNode.next.next)
     self.length -= 1
-        
-  def __str__(self):
-    list = []
-    current_node = self.head
-    for i in range(self.length):
-      list.append(current_node.value)
-      current_node = current_node.next
-    return str(list)
     
   def addNodeAfter(self, new_value, after_node):
     self.checkIndex(after_node)
@@ -61,29 +70,27 @@ class LinkedList(object):
       PriorNode.changeNext(Node(new_value, PriorNode.next))
     self.length +=1
     
-  # Finds particular node in linked list given an index
-  def findNode(self, index):  
-    current_node = self.head
-    i = 0
-    while i < index:
-      current_node = current_node.next
-      i += 1
-    return current_node
-    
   def removeNodesByValue(self, value):
     for i in range(self.length)[::-1]:
       if self.findNode(i).value == value: self.removeNode(i)
       
-ll = LinkedList(1)
+  def reverse(self):
+    for i in range(self.length)[::-1]:
+      self.addNode(self.findNode(i).value)
+    for i in range(self.length/2):
+      self.removeNode(0)
+    
+ll = LinkedList(0)
 print ll # 1
+ll.addNode(0)
+print ll
 ll.addNode(1)
 print ll #12
-ll.addNode(1) 
+ll.addNode(2) 
 print ll # 123
-ll.addNode(1) #123
+ll.addNode(3) #123
 print ll
-ll.addNode(1)
+print ll.head.next.next
+ll.head.next.next.changeNext(ll.head)
 print ll
-ll.removeNodesByValue(2)
-print ll
-print ll.length
+
